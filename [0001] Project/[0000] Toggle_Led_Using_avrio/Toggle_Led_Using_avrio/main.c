b1ca3fa7f9a2c015e85308e63aa9d4b01a8fd141
Khalid Metwally <khalidmetwally64@gmail.com>
2024-10-03 04:40:37 +0300

    update

---
+++ [0001] Project/[0000] Toggle_Led_Using_avrio/Toggle_Led_Using_avrio/main.c
@@ -0,0 +1,21 @@
+/*
+ * Toggle_Led_Using_avrio.c
+ *
+ * Created: 9/30/2024 5:35:22 PM
+ * Author : Khalid Metwally
+ */ 
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRA = 0b00000001 ;
+	while(1)
+	{
+		PORTA ^= (1<<0);
+		_delay_ms(1000);
+	}
+	return 0;
+}
