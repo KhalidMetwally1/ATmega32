cdc6f3f76daba1b3b294f539699cf5ad4c0a17a8
KhalidMetwally1 <khalidmetwally64@gmail.com>
2024-11-15 17:59:18 +0200

    update

---
+++ Drivers/HAL/KPD/KPD_interface.h
@@ -0,0 +1,16 @@
+/*
+ * KPD_interface.h
+ *
+ *  Author: Khalid Metwally
+ *   Layer: HAL
+ *	   SWC: KPD
+ */
+#ifndef KPD_INTERFACE_H_
+#define KPD_INTERFACE_H_
+
+ #define NOTPRESSED 0xff
+
+ void KPD_voidInit(void);		/* OR use PORT Driver	*/
+ u8   KPD_u8GetPressed( void );	/* Gets the pressed key */
+ 
+#endif
---
+++ Drivers/HAL/KPD/KPD_private.h
@@ -0,0 +1,11 @@
+/*
+ * KPD_private.h
+ *
+ *  Author: Khalid Metwally
+ *   Layer: HAL
+ *	   SWC: KPD
+ */
+#ifndef KPD_PRIVATE_H_
+#define KPD_PRIVATE_H_
+
+#endif
